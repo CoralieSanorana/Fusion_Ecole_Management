acd6eae7cb63ce68626a91e03e7c064ac4859ec9
funaki302 <rafanomezantsoafunakilive@gmail.com>
2026-07-04 20:46:16 +0300

    Dashbord bien fait et dynamiser

---
+++ src/main/java/com/ecole/dto/Directeur/DashboardMonthOptionDTO.java
@@ -0,0 +1,10 @@
+package com.ecole.dto.Directeur;
+
+import lombok.Data;
+
+@Data
+public class DashboardMonthOptionDTO {
+    private String value;
+    private String label;
+    private boolean selected;
+}
