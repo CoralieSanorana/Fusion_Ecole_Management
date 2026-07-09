f03266f5a0656c3165776147e7ded7ef06d31d0a
Iavo1926 <maxrazaf622@gmail.com>
2026-07-09 10:17:53 +0300

    c
    d

--- src/main/java/com/ecole/repository/SupportCoursRepository.java
+++ src/main/java/com/ecole/repository/SupportCoursRepository.java
@@ -2,6 +2,7 @@ package com.ecole.repository;
 
 import com.ecole.entity.SupportCours;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface SupportCoursRepository extends JpaRepository<SupportCours, Integer> {
+public interface SupportCoursRepository extends JpaRepository<SupportCours, Integer>,
+        JpaSpecificationExecutor<SupportCours> {
     @Query("""
         SELECT s
         FROM SupportCours s
